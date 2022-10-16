34c9669bf84e4f9f9ffbd1b4d59272e1b67ad805
cjannun <93235878+cjannun@users.noreply.github.com>
2022-10-15 22:34:38 -0400

    Chapter 5
    Completed Chapter 5 files.

---
+++ javascript5/js/main.js
@@ -0,0 +1,8 @@
+// Math Methods and Properties
+
+// From 1 to 10
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
