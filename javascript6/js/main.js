5b17072089ce31276172c348a97503f8fa8bf8ec
cjannun <93235878+cjannun@users.noreply.github.com>
2022-10-15 22:54:46 -0400

    Chapter 6
    Completed Chapter 6 files.

---
+++ javascript6/js/main.js
@@ -0,0 +1,10 @@
+// Your First Code Challenge 
+
+// Write code that will return a random letter from your name
+
+const anyName = "Christopher";
+myNumber = Math.floor(Math.random() * anyName.length);
+console.log(anyName.charAt(myNumber));
+console.log(anyName.charAt(myNumber));
+console.log(anyName.charAt(myNumber));
+console.log(anyName.charAt(myNumber));
