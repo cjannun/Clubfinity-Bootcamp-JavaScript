9861eabf3dafd78c047530981e4f51a9466508e1
cjannun <93235878+cjannun@users.noreply.github.com>
2022-10-17 18:04:27 -0400

    Chapter 15
    Completed Chapter 15 files

---
+++ javascript15/js/main.js
@@ -0,0 +1,21 @@
+// Arrays
+
+const equipShelfA = ["basketball", "football", "volleyball"];
+const equipShelfB = ["basketball", "golf balls", "tennis balls"];
+
+const clothesShelfA = ["tank tops", "t-shirts", "jerseys"];
+const clothesShelfB = ["sweat tops", "sweat pants", "hoodies"];
+
+console.log(equipShelfA[1]);
+//or
+console.log(clothesShelfB[0]);
+
+const equipDept = [equipShelfA, equipShelfB];
+const clothesDept = [clothesShelfA, clothesShelfB];
+
+console.log([0][1]);
+console.log(clothesDept[1][0]);
+
+const sportsStore = [equipDept, clothesDept];
+console.log(sportsStore[0][0][1]);
+console.log(sportsStore[1][1][0]);
