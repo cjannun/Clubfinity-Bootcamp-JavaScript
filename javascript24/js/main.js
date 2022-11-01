863acdbf83d79435232c2f853c2f6e9de670e8f5
cjannun <93235878+cjannun@users.noreply.github.com>
2022-10-31 23:16:20 -0400

    Chapter 24
    Completed Chapter 24 tutorial files

---
+++ javascript24/js/main.js
@@ -0,0 +1,8 @@
+// Modules
+
+import * as Guitars from "./guitars.js"
+import User from "./user.js";
+
+console.log(Guitars.playGuitar());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
